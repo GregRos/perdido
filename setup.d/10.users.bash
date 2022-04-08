9b9ccac573b8d84273cc484aa16f26c3085ddf57
GregRos <life.gregr@gmail.com>
2022-04-09 01:06:02 +0300

    handle existing users

--- setup.d/10.users.bash
+++ setup.d/10.users.bash
@@ -8,14 +8,15 @@ exec 2> >(trap "" INT TERM; sed 's/^/[USERS] /' >&2)
 torrenting_group=torrenting
 rpc_group=rpcing
 echo ADDING GROUP
-groupadd $torrenting_group
-groupadd $rpc_group
+for group in $torrenting_group $rpc_group; do
+  getent group $group || groupadd $group
+done
 
 echo CREATING USERS
 for user in gr an nginx flood arr; do
-  useradd -m $user
+  getent passwd $user || useradd -m $user
 done
-useradd -g $torrenting_group -m rtorrent
+getent passwd rtorrent || useradd -g $torrenting_group -m rtorrent
 echo ADDING TO GROUPS
 gpasswd -M gr,an $torrenting_group
 gpasswd -M nginx,rtorrent,gr,an $rpc_group
