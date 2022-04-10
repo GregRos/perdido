1b922734b4a4419c0fa6fdaaa6016ef439ee50fc
GregRos <life.gregr@gmail.com>
2022-04-11 01:04:15 +0300

    rutorrent instead of flood

--- setup.d/33.rutorrent.bash
+++ setup.d/33.rutorrent.bash
@@ -4,11 +4,13 @@ echo --- RTORRENT ---
 exec > >(trap "" INT TERM; sed 's/^/[RTORRENT] /')
 set -ex
 
-mkdir -p /var/www/perdido.bond/rutorrent
-cd /var/www/perdido.bond/rutorrent
+mkdir -p /var/rutorrent/profiles
+chown -R flood:flood /var/rutorrent
+cd /var/www/perdido.bond
 
 git clone https://github.com/Novik/ruTorrent.git rutorrent
 
+ln -sf ./config/rutorrent/config.php /var/www/perdido.bond/rutorrent/
 chown -R flood:flood rutorrent/
 chmod -R 770 rutorrent/
 
