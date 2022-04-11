0a98e96d74149ff882f129466b68da9ca1293a37
GregRos <life.gregr@gmail.com>
2022-04-11 03:30:36 +0300

    Some fixes

--- setup.d/33.rutorrent.bash
+++ setup.d/33.rutorrent.bash
@@ -9,6 +9,7 @@ chown -R nginx:torrenting /var/rutorrent
 www_rutorent=/var/www/perdido.bond/rutorrent
 rm -rf $www_rutorent
 git clone https://github.com/Novik/ruTorrent.git $www_rutorent
+rm -rf /tmp/rutorrent-themes
 mkdir -p /tmp/rutorrent-themes
 git clone https://github.com/artyuum/3rd-party-ruTorrent-Themes /tmp/rutorrent-themes
 mv /tmp/rutorrent-themes/*/ $www_rutorent/plugins/theme/themes/
