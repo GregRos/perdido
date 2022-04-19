398f9550903cba115b8f8de809aa2fa700250b31
GregRos <life.gregr@gmail.com>
2022-04-19 17:45:12 +0300

    Stop jellyfin early

--- setup.d/40.streaming.bash
+++ setup.d/40.streaming.bash
@@ -4,7 +4,7 @@ echo --- JELLYFIN ---
 exec > >(trap "" INT TERM; sed 's/^/[JELLYFIN] /')
 set -ex
 
-
+systemctl stop jellyfin.service || true
 wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add -
 echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
 rm -rf /etc/jellyfin /etc/default/jellyfin
