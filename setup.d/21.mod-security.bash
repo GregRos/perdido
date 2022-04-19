2bef119431d3d3c4932dc9f38fea7170ebc6f2b1
GregRos <life.gregr@gmail.com>
2022-04-20 01:11:19 +0300

    a few fixes

--- setup.d/21.mod-security.bash
+++ setup.d/21.mod-security.bash
@@ -5,7 +5,7 @@ echo --- SETTING UP MOD_SECURITY ---
 exec > >(trap "" INT TERM; sed 's/^/[21 MOD_SECURITY] /')
 set -ex
 echo INSTALLING DEPENDENCIES
-apt-get install bison build-essential ca-certificates curl dh-autoreconf \
+apt-get install -y bison build-essential ca-certificates curl dh-autoreconf \
         doxygen flex gawk git iputils-ping libcurl4-gnutls-dev libexpat1-dev\
         libgeoip-dev liblmdb-dev libpcre3-dev libpcre++-dev libssl-dev libtool\
         libxml2 libxml2-dev libyajl-dev locales lua5.3-dev pkg-config wget zlib1g-dev
@@ -15,7 +15,7 @@ start_dir="$PWD"
 mkdir -p /etc/nginx/modsec
 
 mkdir ~/ccmod
-cd ccmod
+cd ~/ccmod
 
 echo CLONING MODSECURITY
 git clone https://github.com/SpiderLabs/ModSecurity
