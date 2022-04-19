5f234657bb4670444ea9a6806bbea54e062b9bd4
GregRos <life.gregr@gmail.com>
2022-04-20 00:34:33 +0300

    Update package versions

--- setup.d/00.packages.bash
+++ setup.d/00.packages.bash
@@ -9,11 +9,11 @@ apt-get update && apt-get upgrade -y
 apt-get install -y \
   curl wget git gpg screen byobu sudo apt-transport-https tree  \
   iperf3 fish certbot speedtest-cli gcc make g++ automake \
-  libcurl4-gnutls-dev pkg-config nodejs default-jre mediainfo \
+  pkg-config nodejs default-jre mediainfo \
   p7zip-full unrar dirmngr rtorrent \
   php php-geoip php7.4-cli php7.4-json php7.4-curl php7.4-cgi php7.4-mbstring php7.4-fpm sox \
   ffmpeg sqlite3 python fail2ban \
-  libtool libfuzzy-dev ssdeep gettext libcurl4-openssl-dev liblua5.3-dev libpcre3 libpcre3-dev libxml2 libxml2-dev libyajl-dev doxygen libcurl4 libgeoip-dev libssl-dev zlib1g-dev libxslt-dev liblmdb-dev libpcre++-dev libgd-dev \
+  libtool libfuzzy-dev ssdeep gettext liblua5.3-dev libpcre3 libpcre3-dev libxml2 libxml2-dev libyajl-dev doxygen libcurl4 libgeoip-dev libssl-dev zlib1g-dev libxslt-dev liblmdb-dev libpcre++-dev libgd-dev \
   software-properties-common ufw
 
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python2.7
