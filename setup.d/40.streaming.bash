c8a7d7582faf3e1b37ce29611f27fcf6417e5482
GregRos <life.gregr@gmail.com>
2022-04-19 17:52:34 +0300

    create jellyfin var dir

--- setup.d/40.streaming.bash
+++ setup.d/40.streaming.bash
@@ -8,7 +8,7 @@ systemctl stop jellyfin.service || true
 wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add -
 echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
 rm -rf /etc/jellyfin /etc/default/jellyfin
-mkdir -p /etc/jellyfin
+mkdir -p /etc/jellyfin /var/jellyfin
 ln -sf "$(realpath ./config/jellyfin/default)" /etc/jellyfin/
 ln -sf "$(realpath ./config/jellyfin/config)" /etc/jellyfin/
 apt-get update
