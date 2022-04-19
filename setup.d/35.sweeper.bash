97d363c265827510b83f8452eb7d2a20b6441bda
GregRos <life.gregr@gmail.com>
2022-04-20 01:57:39 +0300

    also install for currnet user

--- setup.d/35.sweeper.bash
+++ setup.d/35.sweeper.bash
@@ -19,6 +19,7 @@ mkdir -p /var/sweeper
 chown -R gr:torrenting /opt/sweeper
 chown -R rtorrent:torrenting /var/sweeper
 
+pip3.10 install patool
 runuser -u rtorrent -- pip3.10 install patool
 echo RUNNING TEST
 sweeper -h
