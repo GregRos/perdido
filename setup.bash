9684d2b55d4f99d66f4684f1eb5fcd1e6d25234c
GregRos <life.gregr@gmail.com>
2022-04-20 00:51:21 +0300

    + to *

--- setup.bash
+++ setup.bash
@@ -7,7 +7,7 @@ echo Starting file execution
 min_file="$1"
 for file in ./setup.d/*.bash; do
   if [ -n "$min_file" ]; then
-    script_num="$(basename $file | grep -oh '^\d+')"
+    script_num="$(basename $file | grep -oh '^\d*')"
     if (( script_num < min_file )); then
       continue
     fi
