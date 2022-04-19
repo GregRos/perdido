52ae899d1f28e9b54573dd703ddff48f4557a7ad
GregRos <life.gregr@gmail.com>
2022-04-19 17:59:48 +0300

    dir symlink to file symlink

--- setup.d/40.streaming.bash
+++ setup.d/40.streaming.bash
@@ -8,9 +8,13 @@ systemctl stop jellyfin.service || true
 wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add -
 echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
 rm -rf /etc/jellyfin /etc/default/jellyfin
-mkdir -p /etc/jellyfin /var/jellyfin
-ln -sf "$(realpath ./config/jellyfin/default)" /etc/jellyfin/
-ln -sf "$(realpath ./config/jellyfin/config)" /etc/jellyfin/
+mkdir -p /etc/jellyfin/config /var/jellyfin
+jellyfin_root=$(realpath ./config/jellyfin)
+ln -sf "$jellyfin_root/default" /etc/jellyfin
+ln -sf "$jellyfin_root/config/*" /etc/jellyfin/config
+for file in ./config/jellyfin/config/*; do
+  ln -sf $(realpath "$file") /etc/jellyfin/config/
+done
 apt-get update
 apt-get install jellyfin -y
 ln -sf "$(realpath ./config/jellyfin.service)" /lib/systemd/system/
