651284a0567a980d49f804697c53fbbbcd63f241
GregRos <life.gregr@gmail.com>
2022-04-19 20:38:49 +0300

    chown correctly

--- setup.d/35.sweeper.bash
+++ setup.d/35.sweeper.bash
@@ -6,7 +6,8 @@ set -ex
 rm -rf /opt/sweeper
 git clone https://github.com/GregRos/sweeper.git /opt/sweeper
 ln -sf "$(realpath ./config/sweeper)" /usr/bin/
-chown -R gr:gr /opt/sweeper
+chown -R gr:torrenting /opt/sweeper
+chown -R rtorrent:torrenting /var/sweeper
 sweeper -h
 
 echo --- DONE ---
