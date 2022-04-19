ccffb8d075dbe5b4d153087cfe8d854fba4d2643
GregRos <life.gregr@gmail.com>
2022-04-19 19:04:40 +0300

    tweaked jellyfin install with default dbs

--- setup.d/40.streaming.bash
+++ setup.d/40.streaming.bash
@@ -8,10 +8,11 @@ systemctl stop jellyfin.service || true
 wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add -
 echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
 rm -rf /etc/jellyfin /etc/default/jellyfin
-mkdir -p /etc/jellyfin/config /var/jellyfin
+mkdir -p /etc/jellyfin/config /var/jellyfin/data/data
 jellyfin_root=$(realpath ./config/jellyfin)
 ln -sf "$jellyfin_root/default" /etc/jellyfin
 cp -f "$jellyfin_root"/config/* /etc/jellyfin/config
+cp -f ./data/jellyfin/*.db /var/jellyfin/data/data
 apt-get update
 apt-get install jellyfin -y
 ln -sf "$(realpath ./config/jellyfin.service)" /lib/systemd/system/
