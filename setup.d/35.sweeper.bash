a7ae61da4b2f0bb79cca862ab5929192648e8c70
GregRos <life.gregr@gmail.com>
2022-04-19 17:06:44 +0300

    gr own all sweeper

--- setup.d/35.sweeper.bash
+++ setup.d/35.sweeper.bash
@@ -6,6 +6,7 @@ set -ex
 rm -rf /opt/sweeper
 git clone https://github.com/GregRos/sweeper.git /opt/sweeper
 ln -sf "$(realpath ./config/sweeper)" /usr/bin/
+chown -R gr:gr /opt/sweeper
 sweeper -h
 
 echo --- DONE ---
