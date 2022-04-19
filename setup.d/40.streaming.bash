b544b675e397c6081fa721f4a8436befcf1eccdc
GregRos <life.gregr@gmail.com>
2022-04-19 17:44:22 +0300

    jellyfin settings, dirs, and service updates

--- setup.d/40.streaming.bash
+++ setup.d/40.streaming.bash
@@ -7,16 +7,11 @@ set -ex
 
 wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo apt-key add -
 echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | sudo tee /etc/apt/sources.list.d/jellyfin.list
+rm -rf /etc/jellyfin /etc/default/jellyfin
+mkdir -p /etc/jellyfin
+ln -sf "$(realpath ./config/jellyfin/default)" /etc/jellyfin/
+ln -sf "$(realpath ./config/jellyfin/config)" /etc/jellyfin/
 apt-get update -y
 apt-get install jellyfin -y
-
 systemctl enable jellyfin.service
 systemctl start jellyfin.service
-
-
-
-# Program directories
-JELLYFIN_DATA_DIR="/var/lib/jellyfin"
-JELLYFIN_CONFIG_DIR="/etc/jellyfin"
-JELLYFIN_LOG_DIR="/var/log/jellyfin"
-JELLYFIN_CACHE_DIR="/var/cache/jellyfin"
