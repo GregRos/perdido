46147414c0ab0096b6890888b2dfa98cc8b62ba8
GregRos <life.gregr@gmail.com>
2022-04-19 19:26:49 +0300

    fixed config

--- setup.d/50.ftp.bash
+++ setup.d/50.ftp.bash
@@ -11,4 +11,7 @@ mkdir -p /etc/vsftpd
 ln -sf "$(realpath ./config/vsftpd/vsftpd.conf)" /etc/vsftpd.conf
 ln -sf "$(realpath ./config/vsftpd/user_list)" /etc/vsftpd
 
+systemctl daemon-reload
+systemctl restart vsftpd.service
+systemctl enable vsftpd.service
 echo --- DONE ---
