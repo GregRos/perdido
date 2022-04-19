eba269353e6bdbcb8be64ca5a253556c4d572c69
GregRos <life.gregr@gmail.com>
2022-04-20 01:10:14 +0300

    removed bad package

--- setup.d/21.mod-security.bash
+++ setup.d/21.mod-security.bash
@@ -8,7 +8,7 @@ echo INSTALLING DEPENDENCIES
 apt-get install bison build-essential ca-certificates curl dh-autoreconf \
         doxygen flex gawk git iputils-ping libcurl4-gnutls-dev libexpat1-dev\
         libgeoip-dev liblmdb-dev libpcre3-dev libpcre++-dev libssl-dev libtool\
-        libxml2 libxml2-dev libyajl-dev locales lua5.3-dev pkg-config wget zlib1g-dev zlibc
+        libxml2 libxml2-dev libyajl-dev locales lua5.3-dev pkg-config wget zlib1g-dev
 
 echo DIR SETUP
 start_dir="$PWD"
