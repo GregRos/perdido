395bffda824dd6982fd10fd304d244766bd4c99b
GregRos <life.gregr@gmail.com>
2022-04-20 00:48:07 +0300

    checkpoint

--- setup.bash
+++ setup.bash
@@ -4,7 +4,15 @@ echo --- BASIC PACKAGES ---
 export DEBIAN_FRONTEND=noninteractive
 
 echo Starting file execution
+
+min_file="$1"
 for file in ./setup.d/*.bash; do
+  if [ -n "$min_file" ]; then
+    script_num="$(echo $file | grep -oh '^\d+')"
+    if (( script_num < min_file )); then
+      continue
+    fi
+  fi
   # guaranteed to be alphabetical, which is why we do the above...
   if ! bash "$file"; then
     echo Error executing $file
