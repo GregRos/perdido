cf9e799685a6aae228bf8cac77a33bb229de1bdf
GregRos <life.gregr@gmail.com>
2022-10-06 03:58:39 +0300

    Changed iperf client stuff

--- run-iperf-test.cmd
+++ run-iperf-test.cmd
@@ -1,2 +1,2 @@
 
-iperf3 -c perdido.bond -p 7871 --username gr --rsa-public-key-path ./perdido.iperf.pem --reverse -t 36000
+iperf3 -c perdido.bond -p 7871 --username gr --rsa-public-key-path ./perdido.iperf.public.pem --reverse -t 36000
