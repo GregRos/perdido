4b0b01f15bc6c06d3d3492d13539c2bedd53711f
GregRos <life.gregr@gmail.com>
2022-10-06 03:51:48 +0300

    Changed iperf client stuff

--- get-iperf-key.cmd
+++ get-iperf-key.cmd
@@ -1,5 +1,5 @@
 echo GETTING IPERF PUBLIC KEY VIA SSH
 
-ssh gr@perdido.bond "cat /etc/iperf/rsa/public.pem" > ./perdido.iperf.public.pem
+ssh gr@perdido.bond "sudo cat /etc/iperf/rsa/public.pem" > ./perdido.iperf.public.pem
 
 echo WROTE TO ./perdido.iperf.public.pem
--- run-iperf-test.cmd
+++ run-iperf-test.cmd
@@ -1,3 +1,2 @@
-.\get-iperf-key.cmd
 
 iperf3 -c perdido.bond -p 7871 --username gr --rsa-public-key-path ./perdido.iperf.pem --reverse -t 36000
