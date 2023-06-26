f6a9611bc3690d4c52b60dd584f7d5d1b19b3adb
GregRos <life.gregr@gmail.com>
2023-06-26 16:48:07 +0300

    Minecraft and factorio updates

--- config/restic/prune.bash
+++ config/restic/prune.bash
@@ -1,34 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-restic forget --tag minecraft-world \
-              --keep-last 5 \
-              --keep-hourly-within 1d \
-              --keep-daily-within 7d \
-              --keep-weekly-within 30d \
-              --keep-monthly-within 1y \
-              --keep-yearly-within 100y
 
-restic forget --tag minecraft \
-              --keep-last 5 \
-              --keep-hourly-within 4h \
-              --keep-daily-within 4d \
-              --keep-weekly-within 14d \
-              --keep-monthly-within 4m \
-              --keep-yearly-within 100y
 
-restic forget --tag factorio \
-              --keep-last 5 \
-              --keep-hourly-within 1d \
-              --keep-daily-within 7d \
-              --keep-weekly-within 30d \
-              --keep-monthly-within 1y \
-              --keep-yearly-within 100y
-
-restic forget --tag factorio-world \
-              --keep-last 5 \
-              --keep-hourly-within 1d \
-              --keep-daily-within 7d \
-              --keep-weekly-within 30d \
-              --keep-monthly-within 1y \
-              --keep-yearly-within 100y
+x
--- setup.d/85.minecraft.bash
+++ setup.d/85.minecraft.bash
@@ -1,24 +1,35 @@
 set -ex
 
 echo SETTING UP CD
+rm -f /bin/minecraft-sync || true
+echo "#!/bin/bash
+sudo -u minecraft-modpack /bin/bash /opt/perdido/shell/minecraft-sync
+" > /bin/minecraft-sync
 
 rm -f /bin/minecraft-redeploy || true
 echo "#!/bin/bash
 sudo -u docker /bin/bash /opt/perdido/shell/minecraft-redeploy
 " > /bin/minecraft-redeploy
 
-chown minecraft-modpack:minecraft-modpack /bin/minecraft-redeploy
-chmod 750 /bin/minecraft-redeploy
-
+rm -f /bin/minecraft-update || true
+echo "#!/bin/bash
+minecraft-sync
+minecraft-redeploy
+" > /bin/minecraft-update
+for command in minecraft-sync minecraft-redeploy minecraft-update; do
+  chown minecraft-modpack:minecraft-modpack /bin/$command
+  chmod 750 /bin/$command
+done
 echo '
+  %minecraft-modpack ALL=(minecraft-modpack) NOPASSWD: /bin/bash /opt/perdido/shell/minecraft-sync
   %minecraft-modpack ALL=(docker) NOPASSWD: /bin/bash /opt/perdido/shell/minecraft-redeploy
 ' | sudo tee /etc/sudoers.d/minecraft
 
 
+echo SETTING UP CONTAINERS
 config=$PWD/config/minecraft
 cd ./config/minecraft
 echo BUILDING THE DOCKER IMAGE
-
 docker build -t my-minecraft .
 
 echo SETTING UP BACKUP CRONJOBS
@@ -26,7 +37,13 @@ rm -f /etc/cron.d/backup-minecraft || true
 cp "$(realpath ./backup.cronjob)" /etc/cron.d/backup-minecraft
 chown root:root /etc/cron.d/backup-minecraft
 
-echo RUNNING REDEPLOY
+echo PUBLISHING MODPACK
+rm -f /data/gregros.dev/xyz/{minecraft-perdido-modpack,minecraft/perdido-modpack} || true
+mkdir -p /data/gregros.dev/xyz/minecraft
+ln -sf /opt/minecraft-perdido-modpack /data/gregros.dev/xyz/minecraft/perdido-modpack
+
+echo RUNNING SYNC AND DEPLOY
+minecraft-sync
 minecraft-redeploy
 
 echo OPENING PORTS
