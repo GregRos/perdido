5ea9d439bc4671d6ef8656daf59f602c9d7fe3f3
GregRos <life.gregr@gmail.com>
2024-01-13 19:08:59 +0200

    fix: compile python with more things

--- setup.d/28.python310.bash
+++ setup.d/28.python310.bash
@@ -4,13 +4,14 @@ set -ex
 # Sweeper is written in Python 3.10. I could rewrite it but I like some of the
 # typing features they added, so I'm going to compile Python 3.10 instead.
 echo DOWNLOADING SOURCE
+sudo apt-get install sabnzbdplus libffi-dev libbz2-dev
 
 wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz
 tar xzf Python-3.10.4.tgz
 cd Python-3.10.4
 
 echo RUNNING CONFIGURE
-./configure --enable-optimizations
+./configure --enable-optimizations --with-system-ffi
 
 echo INSTALL and BUILD
 
