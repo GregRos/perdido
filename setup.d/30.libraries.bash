337d22ad311cc4242a754e303d42f95f6171679d
GregRos <life.gregr@gmail.com>
2024-01-13 19:10:10 +0200

    feature: add usenet dirs

--- setup.d/30.libraries.bash
+++ setup.d/30.libraries.bash
@@ -3,7 +3,7 @@
 set -ex
 
 echo CREATING DATA FOLDER STRUCTURE
-mkdir -p /data/{downloads/{done,going},library/{movies,anime,shows,books,other,programs,trash}}
+mkdir -p /data/{{downloads,usenet}/{done,going},library/{movies,anime,shows,books,other,programs,trash}}
 chown -R rtorrent:torrenting /data
 chmod 770 /data
 
