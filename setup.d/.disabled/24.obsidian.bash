8b3862da4bca9293d8e64d8ed4ae0eadce657962
GregRos <life.gregr@gmail.com>
2025-01-12 16:21:26 +0200

    setup(NOT USED): updated modsecurity, obsidian routing

---
+++ setup.d/.disabled/24.obsidian.bash
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -ex
+echo "!! SETS UP OBSIDIAN ROUTING SYSTEM !!"
+apt-get install -y --no-install-recommends \
+  nginx-core nginx-common nginx nginx-full \
+  python3-certbot-nginx apache2-utils \
+  libnginx-mod-http-dav-ext
+
+my_nginx=$(realpath "./config/nginx")
+mkdir -p /etc/nginx{,www,snippets,conf.d}
+local_nginx=/etc/nginx
+local_www=/var/www/perdido.bond
+unlink $local_nginx/sites-enabled/default || true
+
+echo LINKING STATIC CONTENT
+mkdir -p $local_www
+ln -sf $my_nginx/www/* $local_www
+chown -R nginx:nginx "$local_www"
+
+echo SETTING UP NGINX CONFIG
+sed -i 's/user .*$/user nginx;/im' $local_nginx/nginx.conf
+rm -rf "${local_nginx:?}"/{snippets,conf.d,fragments}
+mkdir -p "$local_nginx"/{snippets,conf.d}
+
+ln -sf "$my_nginx"/conf/*.conf $local_nginx/conf.d/
+ln -sf $my_nginx/snippets/*.conf $local_nginx/snippets/
+
+ln -sf $my_nginx/nginx.service /lib/systemd/system/
+ln -sf "$my_nginx/ssl-dhparams.certbot.pem" $local_nginx
+rm /etc/fail2ban/{jail.d,filter.d}/nginx-*.conf || true
+
+cp -f ./config/fail2ban/filter.d/nginx-*.conf /etc/fail2ban/filter.d/
+cp -f ./config/fail2ban/jail.d/nginx-*.conf /etc/fail2ban/jail.d/
+
+echo GENERATING SECRET
+cp -f $my_nginx/secret.conf $local_nginx/snippets/secret.conf;
+random_token="$(od -vAn -N4 -tu4 < /dev/urandom | md5sum | cut -f1 -d' ')"
+sed -i "s/>>RANDOM<</$random_token/mig" $local_nginx/snippets/secret.conf
+
+echo RELOADING NGINX
+
+systemctl daemon-reload
+systemctl restart nginx
+nginx -t && nginx -s reload
+if ! curl localhost; then
+    echo >&2 nginx seems to be broken
+    exit 3
+fi
+
--- setup.d/.disabled/25.modsecurity.bash
+++ setup.d/.disabled/25.modsecurity.bash
@@ -2,7 +2,7 @@
 set -ex
 return 0
 echo INSTALLING DEPENDENCIES
-apt-get install -y bison build-essential ca-certificates curl dh-autoreconf \
+apt-get install -y bison  dh-autoreconf \
         doxygen flex gawk git iputils-ping libcurl4-gnutls-dev libexpat1-dev\
         libgeoip-dev liblmdb-dev libpcre3-dev libpcre++-dev libssl-dev libtool\
         libxml2 libxml2-dev libyajl-dev locales lua5.3-dev pkg-config wget zlib1g-dev
