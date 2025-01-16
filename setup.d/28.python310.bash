7635f6634a96ff51cab768abfbe67c0691f092f1
GregRos <life.gregr@gmail.com>
2025-01-16 18:22:41 +0200

    fix(python3.10): added sqlite extensions

--- setup.d/28.python310.bash
+++ setup.d/28.python310.bash
@@ -4,14 +4,14 @@ set -ex
 # Sweeper is written in Python 3.10. I could rewrite it but I like some of the
 # typing features they added, so I'm going to compile Python 3.10 instead.
 echo DOWNLOADING SOURCE
-sudo apt-get install sabnzbdplus libffi-dev libbz2-dev
+sudo apt-get install sabnzbdplus libffi-dev libbz2-dev libsqlite3-dev -y
 
 wget https://www.python.org/ftp/python/3.10.4/Python-3.10.4.tgz
 tar xzf Python-3.10.4.tgz
 cd Python-3.10.4
 
 echo RUNNING CONFIGURE
-./configure --enable-optimizations --with-system-ffi
+./configure --enable-optimizations --with-system-ffi --enable-loadable-sqlite-extensions
 
 echo INSTALL and BUILD
 
