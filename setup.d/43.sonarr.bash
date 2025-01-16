7a988cadfa74cef4c6551c0d856b5dd32ee5a810
GregRos <life.gregr@gmail.com>
2025-01-16 18:21:33 +0200

    update(sonarr): v4

--- setup.d/43.sonarr.bash
+++ setup.d/43.sonarr.bash
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 set -ex
+
 systemctl stop sonarr.service || true
-wget https://download.sonarr.tv/v3/main/3.0.10.1567/Sonarr.main.3.0.10.1567.linux.tar.gz
-tar -xvzf Sonarr.main.3.0.10.1567.linux.tar.gz
-rm Sonarr.main.3.0.10.1567.linux.tar.gz
+ln -sf "$(realpath ./config/arr/sonarr.service)" /etc/systemd/system/
+tmpdir=/tmp/install_sonarr
+mkdir -p $tmpdir
+cd $tmpdir
+rm -rf Sonarr.tar.gz Sonarr
+wget "https://services.sonarr.tv/v1/download/main/latest?version=4&os=linux&arch=x64" -O Sonarr.tar.gz
+tar -xvzf  Sonarr.tar.gz
 rm -rf /opt/sonarr
 mv Sonarr /opt/sonarr/
-chmod +x /opt/sonarr/Sonarr.exe
-ln -sf "$(realpath ./config/arr/sonarr.service)" /etc/systemd/system/
+chmod +x /opt/sonarr/Sonarr
+
 systemctl stop sonarr.service || true
 systemctl daemon-reload
 systemctl start sonarr.service
